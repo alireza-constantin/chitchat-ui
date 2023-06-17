00401b934ee90d2532034f792d177a225d0f739a
alireza <alirezasoheili1224@yahoo.com>
2023-06-17 12:12:00 +0330

    scaffold project with tailwind and daisyui

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <div className='text-center btn-accent text-slate-700 w-1/6'  >Testing tailwind</div>
+  </React.StrictMode>,
+)
