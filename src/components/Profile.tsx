bb26c70c0cf2bdc02b0cb273128578ae8473573c
alireza <alirezasoheili1224@yahoo.com>
2023-06-18 15:01:06 +0330

    impl hidden scrolling and fix the opening when resizing the windows

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,11 +1,10 @@
-import { clsx } from "clsx"
 
 export function Profile({ expanded }: { expanded: boolean }) {
     return (
         <div className="flex gap-4 items-center bg-zinc-900 py-3 h-16 px-5 justify-start">
             <ProfilePic />
             {expanded && (
-                <div className="w-3/4 transition-all duration-1000 animate-[unhide_200ms_ease-in-out_forwards]">
+                <div className="w-3/4 transition-all duration-1000 animate-[unhide_300ms_ease-in-out_forwards]">
                     <p className="text-gray-100 text-sm font-semibold pb-1">Jimmy Anderson</p>
                     <p className="text-xs text-gray-300 truncate">last test expanded</p>
                 </div>
